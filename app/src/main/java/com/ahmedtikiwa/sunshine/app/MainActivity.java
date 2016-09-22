d70b6b7e2f8ec67400e8fef2e4b167109626b6c4
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2016-09-22 10:52:36 +0200

    [changes] added fragment with listview

--- app/src/main/java/com/ahmedtikiwa/sunshine/app/MainActivity.java
+++ app/src/main/java/com/ahmedtikiwa/sunshine/app/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ahmedtikiwa.sunshine.app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment, new PlaceHolderFragment())
+                    .commit();
+        }
+    }
+
+    public static class PlaceHolderFragment extends Fragment {
+        public PlaceHolderFragment() {
+
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.fragment_main, viewGroup, false);
+            return view;
+        }
     }
 }
