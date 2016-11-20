262fdc0be533a044865ade3c9afea50bdc252421
Ahmed Tikiwa <ahmed@aewebmasters.co.za>
2016-11-20 17:04:31 +0200

    updated theme colors

--- app/src/main/java/com/ahmedtikiwa/sunshine/app/MainActivity.java
+++ app/src/main/java/com/ahmedtikiwa/sunshine/app/MainActivity.java
@@ -39,8 +39,9 @@ public class MainActivity extends AppCompatActivity implements ForecastFragment.
             }
         } else {
             mTwoPane = false;
+            getSupportActionBar().setElevation(0f);
         }
-        
+
         ForecastFragment forecastFragment = ((ForecastFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_forecast));
         forecastFragment.setUseTodayLayout(!mTwoPane);
     }
